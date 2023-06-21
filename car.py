5f12ead78965a4e08f26238cbce035bd4c24eaa0
Doreen970 <doreenwanyama20@gmail.com>
2023-06-21 18:26:29 +0300

    some changes

--- car.py
+++ car.py
@@ -1,5 +1,12 @@
 from abc import ABC, abstractmethod
 
+def create_car():
+    car_model = input("What is name of the car?: ")
+    car_battery = input("Please write your battery type: ")
+    car_engine = input("What is the engine? :")
+    print(car_model)
+    print(car_battery)
+    print(car_engine)
 
 class Car(ABC):
     def __init__(self, last_service_date):
